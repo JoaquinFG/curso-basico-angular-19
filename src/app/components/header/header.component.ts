cfc59d9516fc924b9c38eac6b26e4b29f0287509
Joaquin Ferreira Garcia <jferreig@ext.unicaja.es>
2025-05-06 08:45:22 +0200

    input output signals

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,62 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, input, OnInit, output } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Menubar } from 'primeng/menubar';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [Menubar],
+  imports: [Menubar, Button],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush, // Cambiamos la estrategia de detección de cambios a OnPush
+  standalone: true, //Angular19 ya por defecto es standalone por lo que no hay que ponerlo
 })
 export class HeaderComponent implements OnInit {
   items: MenuItem[] | undefined;
+  
+  title = input<number>(); // esto es un input signal
+  onEmmit = output<number>(); // esto es un output signal
+
+  //FORMA ANTIGUA 
+  // @Input() title!: string;  // El título que recibes
+  // @Output() onEmmit = new EventEmitter<void>(); // Definimos el Output
+
+  constructor() {
+    // console.log(this.title());
+    // effect(() => {
+    //     console.log(this.title(), 'effect'); // aqui ya está definido el input
+    // });
+  }
 
   ngOnInit() {
-      this.items = [
-          {
-              label: 'Home',
-              icon: 'pi pi-home'
-          },
-          {
-              label: 'Features',
-              icon: 'pi pi-star'
-          },
-          {
-              label: 'Projects',
-              icon: 'pi pi-search',
-              items: [
-                  {
-                      label: 'Components',
-                      icon: 'pi pi-bolt',
-                  },
-                  {
-                      label: 'Blocks',
-                      icon: 'pi pi-server'
-                  },
-                  {
-                      label: 'UI Kit',
-                      icon: 'pi pi-pencil'
-                  },
-                  {
-                      label: 'Templates',
-                      icon: 'pi pi-palette',
-                      items: [
-                          {
-                              label: 'Apollo',
-                              icon: 'pi pi-palette'
-                          },
-                          {
-                              label: 'Ultima',
-                              icon: 'pi pi-palette'
-                          }
-                      ]
-                  }
-              ]
-          },
-          {
-              label: 'Contact',
-              icon: 'pi pi-envelope'
-          }
-      ]
+    // console.log(this.title(), 'ngOnInit');
+
+    this.items = [
+        {
+            label: 'Home',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'Features',
+            icon: 'pi pi-star'
+        },
+        {
+            label: 'Projects',
+            icon: 'pi pi-search',
+            items: [
+                {
+                    label: 'Components',
+                    icon: 'pi pi-bolt',
+                },
+                {
+                    label: 'Blocks',
+                    icon: 'pi pi-server'
+                },
+                {
+                    label: 'UI Kit',
+                    icon: 'pi pi-pencil'
+                },
+                {
+                    label: 'Templates',
+                    icon: 'pi pi-palette',
+                    items: [
+                        {
+                            label: 'Apollo',
+                            icon: 'pi pi-palette'
+                        },
+                        {
+                            label: 'Ultima',
+                            icon: 'pi pi-palette'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Contact',
+            icon: 'pi pi-envelope'
+        }
+    ]
   }
+
+  onClick() {
+    this.onEmmit.emit(1); 
+}
+
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-main',
   imports: [HeaderComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent {}
+export class MainComponent {
+  title = 1;
+
+  emmitReceived(event: number) {
+    this.title = this.title + event;
+    console.log(this.title); // aqui ya está definido el output
+  }
+}
